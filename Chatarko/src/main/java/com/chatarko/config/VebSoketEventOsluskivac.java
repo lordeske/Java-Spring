92b326558dc633e8fc9bced5f23f82024d1be1a5
lordeske8 <eskicmihajlo55@gmail.com>
2024-09-16 23:11:57 +0200

    postavljanje pocetne strane

--- Chatarko/src/main/java/com/chatarko/config/VebSoketEventOsluskivac.java
+++ Chatarko/src/main/java/com/chatarko/config/VebSoketEventOsluskivac.java
@@ -1,9 +1,13 @@
 package com.chatarko.config;
 
 
+import com.chatarko.modeli.ChatPoruka;
+import com.chatarko.modeli.tipPoruke;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -12,10 +16,30 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class VebSoketEventOsluskivac {
 
+
+    private final SimpMessageSendingOperations templatePoruke;
+
+
     @EventListener
     public void handleVebSoketDiskonekt(SessionDisconnectEvent dogadjaj)
     {
-        /// Dodati posle
+        StompHeaderAccessor headerAccessor  = StompHeaderAccessor.wrap(dogadjaj.getMessage());
+        String korisnickoIme = (String) headerAccessor.getSessionAttributes().get("Korisnik");
+
+
+        if(korisnickoIme != null)
+        {
+            log.info("Korisnik se diskonektovao: {}", korisnickoIme);
+            var chatPoruka = ChatPoruka.builder()
+                    .tipPoruke(tipPoruke.IZLAZ)
+                    .posiljalac(korisnickoIme)
+                    .build();
+
+
+            /// mjesto gdje saljemo poruku
+            templatePoruke.convertAndSend("/topic/public", chatPoruka);
+        }
+
     }
 
 
